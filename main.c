e4743e117873e8d959095bf617fbd36a0ff3eb89
soo0300 <soo0300@naver.com>
2022-11-15 17:07:22 +0900

    실습_5(수정)

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
-
-void swap(int *x, int *y){
-	int temp;
-	temp=*x;
-	*x=*y;
-	*y=temp;
-} 
-
-void main(void){
+int main(void){
+	char *pc;
+	int *pi;
+	double *pd;
 	
-	int a=3; int b=5;
-	swap(&a,&b);
-	printf("a: %i\nb: %i",a,b);
+	pc = (char*)10000;
+	pi = (int*)10000;
+	pd = (double *)10000;
+	printf("증가 전:pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	
+	pc++;
+	pi++;
+	pd++;
+	printf("증가 후:pc: = %d, pi = %d, pd %d\n", pc, pi, pd);
 	return 0;
-
 }
+
